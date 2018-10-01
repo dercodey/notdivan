55f862c30a7f157f15431d50efd6276a98777df3
Derek Lane <dglane001@hotmail.com>
2018-10-01 07:56:49 -0500

    adeed comments

--- NotDivan/DbReference.cs
+++ NotDivan/DbReference.cs
@@ -2,10 +2,21 @@
 
 namespace NotDivan
 {
+    /// <summary>
+    /// raw CouchDB reference with revision
+    /// used to return from create or query
+    /// </summary>
     public class DbReference
     {
+        /// <summary>
+        /// document ID is the primary key for accessing the document
+        /// </summary>
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        /// <summary>
+        /// revision for the document reference
+        /// </summary>
         [JsonProperty(PropertyName = "rev")]
         public string RevisionId { get; set; }
     }
